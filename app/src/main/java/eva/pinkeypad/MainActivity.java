2cc15050974b0ce99c8d21fa6ec792762e72713d
Eva Finn <evafinn45@gmail.com>
2017-03-14 12:47:56 +0000

    Initial commit

---
+++ app/src/main/java/eva/pinkeypad/MainActivity.java
@@ -0,0 +1,70 @@
+package eva.pinkeypad;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
+
+    //private Button btn1,btn2,btn3,btn4,btn5,btn6,btn7,btn8,btn9,btn0;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btn1 = (Button)findViewById(R.id.btn1);
+        Button btn2 = (Button)findViewById(R.id.btn2);
+        Button btn3 = (Button)findViewById(R.id.btn3);
+        Button btn4 = (Button)findViewById(R.id.btn4);
+        Button btn5 = (Button)findViewById(R.id.btn5);
+        Button btn6 = (Button)findViewById(R.id.btn6);
+        Button btn7 = (Button)findViewById(R.id.btn7);
+        Button btn8 = (Button)findViewById(R.id.btn8);
+        Button btn9 = (Button)findViewById(R.id.btn9);
+        Button btn0 = (Button)findViewById(R.id.btn0);
+        Button btnclear = (Button)findViewById(R.id.btnclear);
+
+        btn1.setOnClickListener(this);
+        btn2.setOnClickListener(this);
+        btn3.setOnClickListener(this);
+        btn4.setOnClickListener(this);
+        btn5.setOnClickListener(this);
+        btn6.setOnClickListener(this);
+        btn7.setOnClickListener(this);
+        btn8.setOnClickListener(this);
+        btn9.setOnClickListener(this);
+        btn0.setOnClickListener(this);
+        btnclear.setOnClickListener(this);
+
+    }
+
+    @Override
+    public void onClick(View view){
+
+    }
+
+    /**@Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }*/
+}
